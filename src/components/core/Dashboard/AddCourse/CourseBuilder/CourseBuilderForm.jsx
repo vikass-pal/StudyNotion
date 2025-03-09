dc27b999af5ae6025593dbdd914f8139b41ff059
Vikas Pal <vikas.pal_2022@ltce.in>
2025-03-09 22:43:56 +0530

    Started working on step2

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -1,8 +1,63 @@
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+import { IoIosArrowForward } from "react-icons/io";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { useDispatch, useSelector } from 'react-redux';
+import { createSection, updateSection } from '../../../../../services/operations/courseDetailsAPI';
 
 const CourseBuilderForm = () => {
   const {register, handleSubmit, setValue, formState: {errors} } = useForm();
   const [editSectionName, setEditSectionName] = useState(null);
+  const {course} = useSelector((state) => state.course);
+  const dispatch = useDispatch();
+  const goBack = () => {
+    dispatch(setStep(1));
+    dispatch(setEditCourse(true));
+  }
+  const [loading, setLoading] = useState(false);
+  const {token} = useSelector((state) => state.auth);
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+    let result;
+
+    if(editSectionName) {
+      result = await updateSection(
+        {
+          sectionName: data.sectionName,
+          sectionId: editSectionName,
+          courseId: course._id,
+        }, token
+      )
+    }
+    else {
+      result = await createSection(
+        {
+          sectionName: data.sectionName,
+          courseId: course._id,
+        },token
+      )
+    }
+  }
+
+  const goToNext = () => {
+    if(course.courseContent.length === 0) {
+      toast.error("Please add atleast one Section");
+      return;
+    }
+    if(course.courseContent.some((section) => section.length === 0 )) {
+      toast.error("Please add atleast one lecture in each Section");
+      return;
+    }
+    // if everything is fine goto next step
+    dispatch(setStep(3));
+  }
+  
+
+  const cancelEdit = () => {
+    setEditSectionName(null);
+    setValue("sectionName" ,"")
+  }
 
   return (
     <div className='text-white'>
@@ -23,17 +78,41 @@ const CourseBuilderForm = () => {
             )
           }
         </div>
-        <div>
+        <div className='mt-10 flex'>
           <IconBtn type="Submit"
           text={editSectionName ? "Edit Section Name" : "Create Section"}
           outline={true}
           customClasses={"text-white"}
           >
+            <IoMdAddCircleOutline />
+
 
 
           </IconBtn>
+          {editSectionName && (
+            <button
+            type='button'
+            onClick={cancelEdit}
+            >
+              Cancel Edit
+            </button>
+          )}
         </div>
       </form>
+      {course.courseContent.length > 0 && (
+        <NestedView />
+      )}
+
+      <div>
+        <button className='flex justify-end gap-x-3'
+        onClick={goBack}>
+          Back
+        </button>
+        <IconBtn text="Next" onClick={goToNext}>
+        <IoIosArrowForward />
+        </IconBtn>
+      
+      </div>
 
     </div>
   )
