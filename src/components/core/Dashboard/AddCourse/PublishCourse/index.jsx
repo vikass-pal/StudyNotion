50cd8c8f91f98950c42b55009d0d29e06c3a4293
Vikas Pal <vikas.pal_2022@ltce.in>
2025-03-30 22:18:46 +0530

    completed css of CourseBuilder

--- src/components/core/Dashboard/AddCourse/PublishCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/PublishCourse/index.jsx
@@ -78,6 +78,7 @@ export default function PublishCourse () {
 
             <div>
             <button 
+            
             disabled={loading}
             type='button'
             onClick={goBack}
