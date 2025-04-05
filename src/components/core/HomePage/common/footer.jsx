8d5c393920c6fa9c551a509e10362fbb22e7fa3f
Vikas Pal <vikas.pal_2022@ltce.in>
2025-04-05 19:20:21 +0530

    Updated footer

--- src/components/core/HomePage/common/footer.jsx
+++ src/components/core/HomePage/common/footer.jsx
@@ -176,7 +176,7 @@ const footer = () => {
             <div className="flex ">
               <span> Made with ❤️ By </span>
 
-              <Link to='https://github.com/vkas0' target="__blank" className="text-white hover:underline mr-1"
+              <Link to='https://github.com/vikass-pal' target="__blank" className="text-white hover:underline mr-1"
               >
                 Vikas Pal
               </Link>
@@ -188,7 +188,7 @@ const footer = () => {
             <a href="www.linkedin.com/in/vikas-pal-976936vp" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImLinkedin2 size={17} />
             </a>
-            <a href="https://github.com/vkas0" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/vikass-pal" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImGithub size={17} />
             </a>
           </div>
