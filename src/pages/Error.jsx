9b4fcb3f663b5476dcaf9592e674434f6a14a3a5
Vikas Pal <vikas.pal_2022@ltce.in>
2025-04-07 23:58:55 +0530

    Updated Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
   return (
     <div className='flex items-center justify-center 
-    text-3xl text-red'>
+    text-3xl text-white'>
         Error -404 Not Found
     </div>
   )
