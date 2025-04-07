accb6b8f9e3fd4110dee4ed866b6eaab643f9771
Vikas Pal <vikas.pal_2022@ltce.in>
2025-04-07 23:59:35 +0530

    Updated Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,7 @@ const Error = () => {
   return (
     <div className='flex items-center justify-center 
     text-3xl text-white'>
-        Error -404 Not Found
+        Error-404 Not Found
     </div>
   )
 }
