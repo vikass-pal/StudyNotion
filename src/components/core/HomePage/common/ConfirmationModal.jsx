16094b4a96e368dedde04f7f17f7e8e63d29e118
Vikas Pal <vikas.pal_2022@ltce.in>
2025-04-12 21:04:43 +0530

    solved errors

--- src/components/core/HomePage/common/ConfirmationModal.jsx
+++ src/components/core/HomePage/common/ConfirmationModal.jsx
@@ -3,7 +3,10 @@ import IconBtn from "./IconBtn"
 export default function ConfirmationModal({ modalData }) {
     return (
         <div className="fixed inset-0 z-[1000] mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm">
+
             <div className="w-10/12 max-w-[350px] rounded-lg border border-richblack-400 bg-richblack-800 p-6 overflow-auto">
+            
+
 
                 <p className="text-2xl font-semibold text-richblack-5">
                     {modalData?.text1}
