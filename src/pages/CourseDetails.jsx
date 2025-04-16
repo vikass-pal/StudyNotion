5376f810dab77b8d1c9f691d6d6a82842f7d8685
Vikas Pal <vikas.pal_2022@ltce.in>
2025-04-16 22:39:39 +0530

    css of courseDetails

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react'
-import { BiInfoCircle } from "react-icons/bi"
-import { HiOutlineGlobeAlt } from "react-icons/hi"
 import { useDispatch, useSelector } from 'react-redux'
 import {  useParams, useNavigate } from 'react-router-dom'
 import { buyCourse } from '../services/operations/studentFeaturesAPI'
@@ -22,12 +20,9 @@ import { MdOutlineVerified } from 'react-icons/md'
 import Img from './../components/common/Img';
 import toast from "react-hot-toast"
 
-import { addToCart } from '../slices/cartSlice'
-import { ACCOUNT_TYPE } from '../utils/constants'
 
 
-
-function CourseDetails() {
+const CourseDetails = () => {
       const dispatch = useDispatch();
       const navigate = useNavigate();
       const {loading} = useSelector((state) => state.profile);
@@ -36,8 +31,6 @@ function CourseDetails() {
       const {user} = useSelector((state) => state.profile);
       const {token} = useSelector((state) => state.auth);
       const {courseId} = useParams();
-
-      const [response, setResponse] = useState(null)
 
       const [courseData, setCourseData] = useState(null); 
 
@@ -85,26 +78,6 @@ function CourseDetails() {
 
 
 // TODO update
- 
-    const {
-      _id:course_id,
-      courseName,
-      courseDescription,
-      courseContent,
-      createdBy,
-      ratingAndReviews,
-      thumbnail,
-      price,
-      whatYouWillLearn,
-      instructor,
-      studentsEnrolled,
-      createdAt,
-
-     
-    } = courseData.data?.courseDetails;
-
-
-
     const handleBuyCourse = () => { 
       if(token) {
         buyCourse(token, [courseId], user, navigate, dispatch);
@@ -133,37 +106,37 @@ function CourseDetails() {
         </div>
       )
     }
+    const {
+      _id:course_id,
+      courseName,
+      courseDescription,
+      courseContent,
+      createdBy,
+      ratingAndReviews,
+      thumbnail,
+      price,
+      whatYouWillLearn,
+      instructor,
+      studentsEnrolled,
+      createdAt,
 
-    const handleAddToCart = () => {
-        if (user && user?.accountType === ACCOUNT_TYPE.INSTRUCTOR) {
-          toast.error("You are an Instructor. You can't buy a course.")
-          return
-        }
-        if (token) {
-          dispatch(addToCart(response?.data.courseDetails))
-          return
-        }
-        setConfirmationModal({
-          text1: "You are not logged in!",
-          text2: "Please login to add To Cart",
-          btn1Text: "Login",
-          btn2Text: "Cancel",
-          btn1Handler: () => navigate("/login"),
-          btn2Handler: () => setConfirmationModal(null),
-        })
-      }
+     
+    } = courseData.data?.courseDetails;
 
   return (
-    <>
     
-    <div className={`relative w-full bg-richblack-800`}>
-    {/* Hero Section */}
-    <div className="mx-auto box-content px-4 lg:w-[1260px] 2xl:relative ">
-      <div className="mx-auto grid min-h-[450px] max-w-maxContentTab justify-items-cente py-8 lg:mx-0 lg:justify-items-start lg:py-0 xl:max-w-[810px]">
+    <div className={`relative w-full bg-richblack-800 `}>
+        <div className=" mx-auto box-content px-4 lg:w-[1260px] 2xl:relative translate-y-3 ">
+        <div className=" grid min-h-[450px] max-w-maxContentTab justify-items-cente py-8 lg:mx-0 lg:justify-items-start lg:py-0 xl:max-w-[810px]">
 
         {/* Go back button */}
         <div className="mb-5 lg:mt-10 lg:mb-0 z-[100]  " onClick={() => navigate(-1)}>
           <GiReturnArrow className="w-10 h-10 text-yellow-100 hover:text-yellow-50 cursor-pointer" />
+        </div>
+        <div className='-translate-y-16 translate-x-16 text-richblack-100 flex flex-row lg:gap-0 lg:flex-row lg:gap-x-2'>
+          <div>
+            Home / Learning / 
+          </div> <p className='text-yellow-200'>{courseName}</p>
         </div>
 
         {/* will appear only for small size */}
@@ -175,47 +148,38 @@ function CourseDetails() {
           />
           <div className="absolute bottom-0 left-0 h-full w-full shadow-[#161D29_0px_-64px_36px_-28px_inset]"></div>
         </div>
-
-        {/* Course data */}
-        <div className={`mb-5 flex flex-col justify-center gap-4 py-5 text-lg text-richblack-5`}>
-          <p className="text-4xl font-bold text-richblack-5 sm:text-[42px]">{courseName}</p>
+          {/* course data */}
+          <div  className={`-translate-y-24 flex flex-col justify-center gap-4 py-5 text-lg text-richblack-5 h-[320px]`}>
+        <p  className="text-4xl font-bold text-richblack-5 sm:text-[42px]">{courseName}</p>
           <p className='text-richblack-200'>{courseDescription}</p>
-          <div className="text-md flex flex-wrap items-center gap-2">
-            <span className="text-yellow-25">{avgReviewCount}</span>
-            <RatingStars Review_Count={avgReviewCount} Star_Size={24} />
-            <span>{`(${ratingAndReviews.length} reviews)`}</span>
-            <span>{`${studentsEnrolled.length} students enrolled`}</span>
-          </div>
-          <p className="capitalize "> Created By <span className="font-semibold underline">{instructor.firstName} {instructor.lastName}</span></p>
-          <div className="flex flex-wrap gap-5 text-lg">
-            <p className="flex items-center gap-2">
-              {" "}
-              <BiInfoCircle /> Created at {formatDate(createdAt)}
-            </p>
-            <p className="flex items-center gap-2">{" "} <HiOutlineGlobeAlt /> English</p>
-          </div>
+        <div className="text-md flex flex-wrap items-center gap-2">
+          <span className="text-yellow-25">{avgReviewCount}</span>
+          <RatingStars Review_Count= {avgReviewCount} Star_Size={24} />
+          <span>{`(${ratingAndReviews.length} reviews)`}</span>
+          <span>{`(${studentsEnrolled.length} students enrolled)`}</span>
+        </div>
+        <div>
+          < p className="capitalize ">Created By {`${instructor.firstName}`}</p>
         </div>
 
-        {/* will appear only for small size */}
-        <div className="flex w-full flex-col gap-4 border-y border-y-richblack-500 py-4 lg:hidden">
-          <p className="space-x-3 pb-4 text-3xl font-semibold text-richblack-5">Rs. {price}</p>
-          <button className="yellowButton" onClick={handleBuyCourse}>Buy Now</button>
-          <button onClick={handleAddToCart} className="blackButton">Add to Cart</button>
+        <div className='flex gap-x-3'>
+          <p>Created At: {formatDate(createdAt)}</p>
+          <p className="flex items-center gap-2">Language:{" "} English</p>
+          </div>
         </div>
-      </div>
+        </div>
+        {/* floating */}
+        <div className="right-[1.5rem] top-[60px] mx-auto hidden lg:block lg:absolute min-h-[600px] w-1/3 max-w-[410px] translate-y-24 md:translate-y-0">
 
-      {/* Floating Courses Card */}
-      <div className="right-[1.5rem] top-[60px] mx-auto hidden lg:block lg:absolute min-h-[600px] w-1/3 max-w-[410px] translate-y-24 md:translate-y-0">
-        <CourseDetailsCard
-          course={response?.data?.courseDetails}
-          setConfirmationModal={setConfirmationModal}
-          handleBuyCourse={handleBuyCourse}
-        />
-      </div>
-    </div>
-  </div>
+          <CourseDetailsCard 
+          course = {courseData?.data?.courseDetails}
+          setConfirmationModal = {setConfirmationModal}
+          handleBuyCourse = {handleBuyCourse}
 
-       
+          />
+        </div>
+
+        </div >
         <div className="mx-auto box-content px-4 text-start text-richblack-5 lg:w-[1260px]">
           <div className="mx-auto max-w-maxContentTab lg:mx-0 xl:max-w-[810px]">
         <div className="my-8 border border-richblack-600 -translate-y-16 bg-richblack-900 mx-auto text-richblack-25 w-full p-8">
@@ -231,16 +195,16 @@ function CourseDetails() {
               )}
             </div>
           </div>
-        <div>
-          <div>
-            <p>Course Content :</p>
+        <div className="max-w-[830px] mt-9">
+          <div  className="flex flex-col gap-3">
+            <p className="text-[28px] font-semibold">Course Content :</p>
 
           </div>
           <div className='flex gap-x-3 justify-between'>
            <div>
-           <span>{courseContent.length} section(s)</span>
+           <span>{courseContent.length} {`section(s)`} section(s)</span>
            
-          <span> {totalNoOfLectures}</span>
+          <span> {totalNoOfLectures} {`lecture(s)`}</span>
           <span>
             {courseData.data?.totalDuration} Total length
           </span>
@@ -290,7 +254,8 @@ function CourseDetails() {
           
 
         {confirmationModal && <ConfirmationModal modalData={confirmationModal}/>}
-        </>
+
+    </div>
    
   )
 }
