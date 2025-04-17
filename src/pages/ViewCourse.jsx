b69800f8030881e1ef136298a2e1599fc6c97a85
Vikas Pal <vikas.pal_2022@ltce.in>
2025-04-17 22:15:07 +0530

    Started viewCourse

--- src/pages/ViewCourse.jsx
+++ src/pages/ViewCourse.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import {Outlet, useParams} from 'react-router-dom'
 
 const ViewCourse = () => {
@@ -8,6 +9,21 @@ const [reviewModal, setReviewModal] = useState(false)
 const {courseId} = useParams()
 const {token} = useSelector((state) => state.auth);
 const diapatch = useDispatch();
+
+  useEffect(() => {
+    const setCourseSpecificDetails = async() => {
+      const courseData = await getFullDetailsOfCourse(courseId, token);
+      dispatch(setCourseSectionData(courseData.courseDetails.courseContent));
+      dispatch(setEntireCourseData(courseData.courseDetails));
+      dispatch(setCompletedLectures(courseData.completed)); 
+
+      let lectures = 0;
+      courseData?.courseDetails?.courseContent?.forEach((sec) => {
+        lectures += sec.subSectionlength
+      })
+      dispatch(setTotalNoOfLectures(lectures));
+    }
+  },[]);
  
 
   return (
