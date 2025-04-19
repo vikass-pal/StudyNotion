15d5e676590b4659fd2d7a0cdf792210bb47f72c
Vikas Pal <vikas.pal_2022@ltce.in>
2025-04-19 13:49:18 +0530

    viewCourse modal

--- src/components/core/ViewCourse/CourseReviewModal.jsx
+++ src/components/core/ViewCourse/CourseReviewModal.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { createRating } from '../../../services/operations/courseDetailsAPI';
 
 const CourseReviewModal = ({setReviewModal}) => {
 
 
     const {user} = useSelector((state) => state.profile);
+   const {courseEntireData} = useSelector((state) => state.viewCourse);
     const {token} = useSelector((state) => state.auth);
     const {
         register,
@@ -23,6 +26,15 @@ const CourseReviewModal = ({setReviewModal}) => {
     const ratingChanged = (newRating) => {
         setValue("courseRating", newRating);
 
+    }
+
+    const onSubmit = async (data) => {
+        await createRating({
+            courseId:courseEntireData,
+            rating:data.courseRating,
+            review:data.courseExperience,
+        }, token);
+        setReviewModal(false);
     }
 
   return (
@@ -55,9 +67,28 @@ const CourseReviewModal = ({setReviewModal}) => {
 
                             />
                             <div>
-                                <label htmlFor="">
+                                <label htmlFor="courseExperience">
                                     Add your Experience
                                 </label>
+                                <textarea 
+                                id='courseExperience'
+                                placeholder='Add your experience here'
+                                {...register("courseExperience", {required: true})}
+                                className='form-style min-h-[130px] w-full '
+                                />
+                                {
+                                    errors.courseExperience && (
+                                        <span>
+                                           Please Add your experience here
+                                        </span>
+                                    )
+                                }
+                            </div>
+                            <div>
+                                <button onClick={() => setReviewModal(false)}>Cancel  </button>
+                                <IconBtn 
+                                text="Save"
+                                />
                             </div>
 
                         </form>
