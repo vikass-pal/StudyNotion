ad6ab6326d36675965c3778eef0b83b44de88171
Vikas Pal <vikas.pal_2022@ltce.in>
2025-05-28 23:16:57 +0530

    final errors

--- src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
+++ src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { getInstructorData } from '../../../../services/operations/profileAPI';
 import { fetchInstructorCourses } from '../../../../services/operations/courseDetailsAPI';
 import { useEffect, useState } from "react"
+
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 
